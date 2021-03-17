3e968891a60efdbabcc48c32715fa12287983ef8
bvogels <benjamin.vogels@gmx.de>
2021-03-17 14:47:15 +0100

    4. Test: Added some values for testing. It works, but currently it fails when the original values is 0 mod (10).

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -8,13 +8,30 @@ public class Result {
     }
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
+        int current = 0;
         if (grades.size() > 1 && grades.get(0) <= 60 && grades.get(0) > 0) {
             for (int entry = 1; entry < grades.size(); entry++) {
-                if ( grades.get(entry) > -1 && grades.get(entry) <= 40)
-                    System.out.println("Test");
+                if (grades.get(entry) >= 38 && grades.get(entry) <= 100) {
+                    System.out.println("Current entry: " + grades.get(entry));
+                    int tmp = grades.get(entry) % 10;
+                    if (tmp % 5 == 0) {
+                        break;
+                    }
+                    current = (int) Math.floor(grades.get(entry) / 10);
+                    if (tmp > 2 && tmp < 5) {
+                        current = current * 10 + 5;
+                        System.out.println("Current: " + current + ", tmp: " + tmp);
+                        grades.set(entry, current);
+                    }
+                    else if (tmp > 7 && tmp <= 9) {
+                        current = current * 10 + 10;
+                        System.out.println("Current: " + current + ", tmp: " + tmp);
+                        grades.set(entry, current);
+                    }
+                }
             }
         } else {
-            System.out.println("Contains no grades.");
+            System.out.println("Contains no grades or 60+ students.");
         }
         return grades;
     }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -19,10 +19,31 @@ public class ResultTest {
 
     @Test
     void testCheckResultForValidity1() {
-        List<Integer> z = new ArrayList<>(Arrays.asList(1, 5));
+        List<Integer> z = new ArrayList<>(Arrays.asList(5));
         assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Congruent");
+    }
 
+    @Test
+    void testCheckResultForValidity2() {
+        List<Integer> z = new ArrayList<>(Arrays.asList(61));
+        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Congruent");
+    }
 
+    @Test
+    void testCheckResultForValidity3() {
+        List<Integer> z = new ArrayList<>(Arrays.asList(4, 8, 10, 25, 78));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(4, 8, 10, 25, 80));
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), " Not congruent");
+    }
+
+    @Test
+    void testCheckResultForValidity4() {
+        List<Integer> z = new ArrayList<>(Arrays.asList(7, 39, 41, 42, 57, 88, 99, 1));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(7, 40, 41, 42, 57, 90, 100, 1));
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), " Not congruent");
     }
 
 }
+
+
+
