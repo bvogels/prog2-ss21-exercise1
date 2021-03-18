648773d1265131fff41092511338357dbf640ee6
bvogels <benjamin.vogels@gmx.de>
2021-03-18 13:41:58 +0100

    5., 6. Test: Check if boundary grades, such as 50, are identified correctly. Check if the first entry in the ArrayList (number of students) matches the number of grades.
    Commented the source code.

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,34 +1,49 @@
 import java.util.List;
 
 public class Result {
-    List<Integer> grades;
-
-    public Result (List<Integer> rawData) {
-        grades = rawData;
-    }
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
-        int current = 0;
-        if (grades.size() > 1 && grades.get(0) <= 60 && grades.get(0) > 0) {
+        int current = 0; // This variable holds the grade/10 for further calculation
+
+        /* This if statement test if the ArrayList adheres to the following criteria:
+        1. Comprises more than one entry
+        2. Not more than 60 grades
+        3. The number of grades matches the first entry in the List (number of students)
+        If one condition is not met, the List will be returned unchanged.
+         */
+
+        if (grades.size() > 1 && grades.get(0) <= 60 && grades.get(0) > 0 && grades.size() == grades.get(0)+1) {
+
+            /* This for-loop is run 'entry' times, starting with index 1 */
+
             for (int entry = 1; entry < grades.size(); entry++) {
+
+                /* If the entry is 38 or more and less or equal 100, its elegibility
+                for rounding is checked. */
+
                 if (grades.get(entry) >= 38 && grades.get(entry) <= 100) {
-                    System.out.println("Current entry: " + grades.get(entry));
+
+                    /* The grade is divided mod (10). This gives merely the eligiblity
+                    whether the grade is rounded or not.
+                    The variable 'current' holds the result from division
+                    of the grade by 10. If the grade is rounded, the result of the division
+                    by 10, 5 (or 10) are added and then multiplied by 'current.'
+                     */
                     int tmp = grades.get(entry) % 10;
                     current = (int) Math.floor(grades.get(entry) / 10);
                     if (tmp > 2 && tmp < 5) {
                         current = current * 10 + 5;
-                        System.out.println("Current: " + current + ", tmp: " + tmp);
                         grades.set(entry, current);
                     }
                     else if (tmp > 7 && tmp <= 9) {
                         current = current * 10 + 10;
-                        System.out.println("Current: " + current + ", tmp: " + tmp);
                         grades.set(entry, current);
                     }
                 }
             }
         } else {
-            System.out.println("Contains no grades or 60+ students.");
+            // This line is printed in case of invalid data.
+            System.out.println("Contains no grades, 60+ students, or invalid data.");
         }
         return grades;
     }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Github: https://github.com/bvogels/prog2-ss21-exercise1.git
 
 public class ResultTest {
 
@@ -18,33 +19,49 @@ public class ResultTest {
     @BeforeEach
     void setUp() {}
 
-    @DisplayName("Checks if the list is unchanged in case it is invalid: Only one element.")
+    @DisplayName("Check if the list is unchanged in case it is invalid: Only one element.")
     @Test
     void testCheckResultForValidity1() {
         List<Integer> z = new ArrayList<>(Arrays.asList(5));
-        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Congruent");
+        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Not congruent");
     }
 
-    @DisplayName("Checks if the list is unchanged in case it is invalid: Too much elements.")
+    @DisplayName("Check if the list is unchanged in case it is invalid: Too much elements.")
     @Test
     void testCheckResultForValidity2() {
         List<Integer> z = new ArrayList<>(Arrays.asList(61));
-        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Congruent");
+        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "Not congruent");
     }
 
-    @DisplayName("Checks if grades are rounded correctly (1).")
+    @DisplayName("Check if grades are rounded correctly (1).")
     @Test
     void testCheckResultForValidity3() {
         List<Integer> z = new ArrayList<>(Arrays.asList(4, 8, 10, 25, 78));
         List<Integer> expected = new ArrayList<>(Arrays.asList(4, 8, 10, 25, 80));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), " Not congruent");
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), "Rounded incorrectly");
     }
 
+    @DisplayName("Check if grades are rounded correctly (2).")
     @Test
     void testCheckResultForValidity4() {
-        List<Integer> z = new ArrayList<>(Arrays.asList(8, 39, 41, 42, 57, 60, 88, 99, 1, 50));
-        List<Integer> expected = new ArrayList<>(Arrays.asList(8, 40, 41, 42, 57, 60, 90, 100, 1, 50));
-        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), " Not congruent");
+        List<Integer> z = new ArrayList<>(Arrays.asList(9, 39, 41, 42, 57, 60, 88, 99, 1, 50));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(9, 40, 41, 42, 57, 60, 90, 100, 1, 50));
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), "Rounded incorrectly");
+    }
+
+    @DisplayName("Check for boundary numbers.")
+    @Test
+    void testCheckResultForValidity5() {
+        List<Integer> z = new ArrayList<>(Arrays.asList(4, 0, 30, 40, 100));
+        List<Integer> expected = new ArrayList<>(Arrays.asList(4, 0, 30, 40, 100));
+        assertTrue(Arrays.equals(expected.toArray(), Result.gradingStudents(z).toArray()), "Rounded incorrectly");
+    }
+
+    @DisplayName("Check for coherent data: # of student matches # of grades")
+    @Test
+    void testCheckResultForValidity6() {
+        List<Integer> z = new ArrayList<>(Arrays.asList(3, 51, 52, 53, 54, 55));
+        assertTrue(Arrays.equals(z.toArray(), Result.gradingStudents(z).toArray()), "No match (Students/# of grades)");
     }
 
 }
